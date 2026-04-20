e9e5c518cfa29f6f2da6c5e86b0a5482ce0aa156
Ashirwad Kumar <ashirwadk04@gmail.com>
2026-04-20 12:23:19 +0530

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './Componenets/NavBar';
+import News from './Componenets/News';
 
 export default class App extends Component {
    c="jhon"
   render() {
     return (
       <div>
-        Helo my first class base component {this.c}
+      
+      <NavBar></NavBar>
+      <News></News>
+      
 
       </div>
     )
